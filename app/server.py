e9118f09625e42418f1c7d53611a835916782854
DDB89 <47303519+DDB89@users.noreply.github.com>
2019-05-06 15:52:15 +0300

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from fastai import *
 from fastai.vision import *
 
 # export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_url = 'https://drive.google.com/uc?export=download&id=1Cuim1Ajgmi8gfVHJHpiarNKZLeZkCGaJ'
+export_file_url = 'https://www.dropbox.com/s/c5dp8rvc5ak1u16/export.pkl?dl=1'
 export_file_name = 'export.pkl'
 
 classes = ['Amphibia', 'Birds', 'Mammal', 'Reptile']
