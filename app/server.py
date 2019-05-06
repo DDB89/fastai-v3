4af1c27eff36587a6d1ab26e595238b4886b4724
DDB89 <47303519+DDB89@users.noreply.github.com>
2019-05-06 16:10:17 +0300

    Update server.py

--- app/server.py
+++ app/server.py
@@ -12,7 +12,7 @@ from fastai.vision import *
 export_file_url = 'https://www.dropbox.com/s/c5dp8rvc5ak1u16/export.pkl?dl=1'
 export_file_name = 'export.pkl'
 
-classes = ['Amphibia', 'Birds', 'Mammal', 'Reptile']
+classes = ['Amphibia','Birds','Mammal','Reptile']
 path = Path(__file__).parent
 
 app = Starlette()
